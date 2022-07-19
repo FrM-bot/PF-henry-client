040f21ca0cf66f69143a72eecffe03099928fd03
Matt <maty.britez7@gmail.com>
2022-07-19 13:38:08 -0300

    install axios and add cryptoTables

--- pages/cryptosInfo.js
+++ pages/cryptosInfo.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import TableCoins from '../components/TableCoins'
 
 const CryptosInfo = () => {
-  // eslint-disable-next-line no-unused-vars
   const [coin, setCoin] = useState([])
+  const [search, setSearch] = useState('')
 
   const getData = async () => {
     const res = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
@@ -15,7 +16,11 @@ const CryptosInfo = () => {
     getData()
   }, [])
   return (
-    <div>cryptosInfo</div>
+      <div>
+        <input type="text" placeholder='buscar cryptos' onChange={(e) => { setSearch(e.target.value) }}></input>
+       <h1>Crypto markets</h1>
+       <TableCoins coins={coin} search={search}/>
+      </div>
   )
 }
 
